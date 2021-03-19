6dd19ea3ff51f2fca55ea8af663d4d8cb7543c68
Matheus Palmeira <matheus_160497@hotmail.com>
2021-03-19 03:38:19 -0300

    Added eslint/clean code/refactor/tests ok

--- 2-EXERCISE/src/index.js
+++ 2-EXERCISE/src/index.js
@@ -1,120 +1,157 @@
-function User(score, hasBoost = true) {
-  var history = []
-  var debts = 0
+/* eslint-disable consistent-return */
 
-  this.setScore = function (score) {
-    score = score
-  }
+function User(score = 0, hasBoot = true) {
+  this.debts = 0;
+  this.history = [];
+  this.userScore = score;
+  this.userHasBoost = hasBoot;
 
-  this.setDebts = function (debts) {
-    debts = debts
-  }
+  // eslint-disable-next-line no-nested-ternary
+  const calculateMaxBoost = () => ((this.userScore < 500)
+    ? this.userScore * 0.2
+    : this.userScore < 800
+      ? this.userScore * 0.05
+      : this.userScore * 0.03);
+
+  this.checkBoost = () => {
+    const maxBoost = calculateMaxBoost();
+    const isBroken = this.history.find((h) => h.status === 'broken');
+    const isAccepted = this.history.map((h) => h.status === 'accept');
+    const debtsPaid = isAccepted ? isAccepted.length : 0;
+    const boostData = {
+      available: maxBoost,
+      lost: 0,
+      pending: 0,
+      complete: 0,
+    };
+
+    // Verifica se ele possui o Turbo
+    if (!this.userHasBoost) {
+      return;
+    }
+
+    // Se não possui histórico e possui débitos e possui turbo => Então, possui turbo disponível.
+    if (this.history.length === 0 && this.debts) {
+      return boostData;
+    }
+
+    if (isBroken) {
+      boostData.lost = maxBoost;
+      return boostData;
+    }
+
+    // Caso o consumidor tenha pago todas suas dividas, ele tem um boost completo e ativo!
+    if (isAccepted && this.debts === 0) {
+      boostData.available = 0;
+      boostData.lost = 0;
+      boostData.complete = maxBoost;
+      return boostData;
+    }
+
+    // O consumidor não terminou de pagar suas contas é não quebrou nenhum acordo
+    boostData.available = maxBoost / (this.debts + debtsPaid);
+    boostData.pending = maxBoost - boostData.available;
+    return boostData;
+  };
+
+  const setNewStatus = (boost) => {
+    let newStatus = [5, null];
+
+    if (!boost) {
+      return newStatus;
+    }
+
+    const status = Object.keys(boost);
+    status.forEach((key) => {
+      if (boost[key]) {
+        switch (key) {
+          case 'lost': {
+            newStatus = [2];
+            break;
+          }
+          case 'pending': {
+            newStatus = [3];
+            break;
+          }
+          case 'available': {
+            newStatus = [1];
+            break;
+          }
+          default: {
+            newStatus = [4];
+          }
+        }
+        newStatus.push(boost[key]);
+      }
+    });
+
+    return newStatus;
+  };
+
+  this.statusBoost = () => {
+    const boost = this.checkBoost();
+    return setNewStatus(boost);
+  };
+
+  this.setDebts = (newDebts) => {
+    this.debts = newDebts;
+  };
+
+  this.setScore = (newScore) => {
+    this.userScore = newScore;
+  };
+
+  this.setHasBoot = (newValue) => {
+    this.userHasBoost = newValue;
+  };
 
   this.acceptOffer = () => {
     // Pagou sua negociação
-    debts -= 1
-    history.push({
-      status: 'accept'
-    })
-  }
+    this.debts -= 1;
+    this.history.push({ status: 'accept' });
+  };
 
   this.brokenOffer = () => {
     // Quebrou algum acordo por nao ter pago a dívida ou apareceu mais uma negativaçāo no seu CPF
-    debts += 1
-    history.push({
-      status: 'broken'
-    })
-  }
+    this.debts += 1;
+    this.history.push({ status: 'broken' });
+  };
 
-  this.getScore = function () {
-    var boost = this.statusBoost()
-
-    var boostMessage = ''
-    var boostValue = Math.round(boost[1])
-
-    if (boost[0] == 5) {
-      boostMessage = 'Você não possui turbo disponível'
-    } else if (boost[0] === 1) {
-      boostMessage = 'Você pode aumentar seu Score em até'
-    } else if (boost[0] === 2) {
-      boostMessage = 'Você perdeu o turbo do seu Score'
-    } else if (boost[0] === 3) {
-      boostMessage = 'Você pode aumentar seu Score'
-    } else if (boost[0] === 4) {
-      boostMessage = 'Você ativou o turbo do seu Score'
-    }
-
-    return {
-      score,
-      boost: {
-        status: boost[0],
-        value: boostValue,
-        message: boostMessage,
-      },
-    }
-  }
-
-  this.checkBoost = function () {
-    if (!hasBoost) {
-      // Verifica se ele possui o Turbo
-      return undefined
-    }
-
-    var maxBoost = score < 200 ? score * 0.2 : score < 500 ? score * 0.2 : score < 800 ? score * 0.05 : score * 0.03
-    var available = maxBoost
-    var lost = 0
-    var pending = 0
-    var complete = 0
-
-    if (history.find(h => h.status === 'broken')) {
-      // Caso o consumidor tenha alguma quebra de acordo / alguma divida ele perde o Turbo do Score
-      available = null
-      lost = maxBoost
-    } else if (history.find(h => h.status === 'accept')) {
-      var debtsPaid = history.filter(h => h.status === 'accept').length
-      if (0 == debts) {
-        // Caso o consumidor tenha pago todas suas dividas, ele tem um boost completo e ativo!
-        available = 0
-        complete = maxBoost
-      } else {
-        // O consumidor não terminou de pagar suas contas é não quebrou nenhum acordo
-        available = maxBoost / (debts + debtsPaid)
-        pending = maxBoost - available
+  const generateBoostMessage = (boost) => {
+    switch (boost[0]) {
+      case 1: {
+        return 'Você pode aumentar seu Score em até';
+      }
+      case 2: {
+        return 'Você perdeu o turbo do seu Score';
+      }
+      case 3: {
+        return 'Você pode aumentar seu Score';
+      }
+      case 4: {
+        return 'Você ativou o turbo do seu Score';
+      }
+      default: {
+        return 'Você não possui turbo disponível';
       }
     }
+  };
 
-    return {
-      available,
-      lost,
-      pending,
-      complete,
-    }
-  }
+  this.getScore = () => {
+    const boost = this.statusBoost();
+    const boostValue = Math.round(boost[1]);
+    const boostMessage = generateBoostMessage(boost);
+    const userData = {
+      score: this.userScore,
+      boost: {
+        status: boost[0],
+        value: boostValue === 0 ? null : boostValue,
+        message: boostMessage,
+      },
+    };
 
-  this.statusBoost = function () {
-    var boost = this.checkBoost()
-    /*
-    1 = Disponivel
-    2 = Perdido
-    3 = Pendente
-    4 = Completo
-    5 = Indisponível
-    */
-    if (boost == null) {
-      return [5, null]
-    } else if (boost.lost) {
-      return [2, boost.lost]
-    } else if (boost.pending) {
-      return [3, boost.pending]
-    } else if (boost.available) {
-      return [1, boost.available]
-    } else if (!boost.available && !boost.pending) {
-      return [4, boost.complete]
-    } else {
-      return null
-    }
-  }
+    return userData;
+  };
 }
 
-
-module.exports = User
+module.exports = User;
--- 2-EXERCISE/src/index.test.js
+++ 2-EXERCISE/src/index.test.js
@@ -1,67 +1,68 @@
-const User = require('./index')
+const User = require('./index');
 
 test('Cenario 1: Turbo disponível', () => {
   /**
    * Regras:
    * Consumidor tem 390 de Score de crédito, possui o Serasa Turbo disponível e 5 dívidas
    */
-  const user = new User(390)
-  user.setDebts(5)
+  const user = new User(390);
+  user.setDebts(5);
 
-  const score = user.getScore()
+  const userData = user.getScore();
 
-  expect(score.score).toBe(390)
-  expect(score.boost.status).toBe(1)
-  expect(score.boost.value).toBe(78)
-  expect(score.boost.message).toBe('Você pode aumentar seu Score em até')
-})
+  expect(userData.score).toBe(390);
+  expect(userData.boost.status).toBe(1);
+  expect(userData.boost.value).toBe(78);
+  expect(userData.boost.message).toBe('Você pode aumentar seu Score em até');
+});
 
 test('Cenario 2: Turbo perdido', () => {
   /**
    * Regras:
-   * Consumidor tem 599 de Score de crédito, possui o Serasa Turbo disponível e 5 dívidas
+   * Consumidor tem 559 de Score de crédito, possui o Serasa Turbo disponível e 5 dívidas
    * e essas 5 dívidas foram negociadas mas deixou de pagar e assim quebrou o seu Serasa Turbo.
    */
-  const user = new User(559)
-  user.setDebts(5)
-  user.acceptOffer()
-  user.acceptOffer()
-  user.acceptOffer()
-  user.acceptOffer()
+  const user = new User(559);
+  user.setDebts(5);
+  user.acceptOffer();
+  user.acceptOffer();
+  user.acceptOffer();
+  user.acceptOffer();
 
-  var score = user.getScore()
-  expect(score.boost.status).toBe(3)
+  let userData = user.getScore();
+  expect(userData.boost.status).toBe(3);
 
-  user.acceptOffer()
-  score = user.getScore()
-  expect(score.boost.status).toBe(4)
+  user.acceptOffer();
+  userData = user.getScore();
+  expect(userData.boost.status).toBe(4);
 
-  user.brokenOffer()
-  score = user.getScore()
+  user.brokenOffer();
+  userData = user.getScore();
 
-  expect(score.score).toBe(559)
-  expect(score.boost.status).toBe(2)
-  expect(score.boost.value).toBe(28)
-  expect(score.boost.message).toBe('Você perdeu o turbo do seu Score')
-})
+  expect(userData.score).toBe(559);
+  expect(userData.boost.status).toBe(2);
+  expect(userData.boost.value).toBe(28);
+  expect(userData.boost.message).toBe('Você perdeu o turbo do seu Score');
+});
 
 test('Cenario 3: Turbo a liberar', () => {
   /**
    * Regras:
-   * Consumidor tem 200 de Score de crédito, possui o Serasa Turbo disponível e 2 dívidas
-   * e uma divida foi negociada a outra ainda é possível liberar os pontos disponíveis no seu Serasa Turbo
+   * Consumidor tem 200 de Score de crédito, possui o Serasa Turbo disponível e
+   * 2 dívidas e uma divida foi negociada a outra ainda é possível liberar os
+   * pontos disponíveis no seu Serasa Turbo
    */
-  const user = new User(200)
-  user.setDebts(2)
-  user.acceptOffer()
+  const user = new User(200);
+  user.setDebts(2);
+  user.acceptOffer();
 
-  const score = user.getScore()
+  const userData = user.getScore();
 
-  expect(score.score).toBe(200)
-  expect(score.boost.status).toBe(3)
-  expect(score.boost.value).toBe(20)
-  expect(score.boost.message).toBe('Você pode aumentar seu Score')
-})
+  expect(userData.score).toBe(200);
+  expect(userData.boost.status).toBe(3);
+  expect(userData.boost.value).toBe(20);
+  expect(userData.boost.message).toBe('Você pode aumentar seu Score');
+});
 
 test('Cenario 4: Turbo ativo', () => {
   /**
@@ -69,19 +70,19 @@ test('Cenario 4: Turbo ativo', () => {
    * Consumidor tem 359 de Score de crédito, possui o Serasa Turbo disponível e 3 dívidas
    * e as 3 dividas foram negocias e o consumidor ganhou seus pontos no Serasa Turbo
    */
-  const user = new User(359)
-  user.setDebts(3)
-  user.acceptOffer()
-  user.acceptOffer()
-  user.acceptOffer()
+  const user = new User(359);
+  user.setDebts(3);
+  user.acceptOffer();
+  user.acceptOffer();
+  user.acceptOffer();
 
-  const score = user.getScore()
+  const userData = user.getScore();
 
-  expect(score.score).toBe(359)
-  expect(score.boost.status).toBe(4)
-  expect(score.boost.value).toBe(72)
-  expect(score.boost.message).toBe('Você ativou o turbo do seu Score')
-})
+  expect(userData.score).toBe(359);
+  expect(userData.boost.status).toBe(4);
+  expect(userData.boost.value).toBe(72);
+  expect(userData.boost.message).toBe('Você ativou o turbo do seu Score');
+});
 
 test('Cenario 5: Turbo indisponível', () => {
   /**
@@ -89,12 +90,12 @@ test('Cenario 5: Turbo indisponível', () => {
    * Consumidor tem 787 de Score de crédito mas não está elegível para o Serasa Turbo,
    * pois ele não ativou seu Cadastro Positivo e assim ele não tem pontos disponíveis.
    */
-  const user = new User(787, false)
+  const user = new User(787, false);
 
-  const score = user.getScore()
+  const userData = user.getScore();
 
-  expect(score.score).toBe(787)
-  expect(score.boost.status).toBe(5)
-  expect(score.boost.value).toBe(null)
-  expect(score.boost.message).toBe('Você não possui turbo disponível')
-})
+  expect(userData.score).toBe(787);
+  expect(userData.boost.status).toBe(5);
+  expect(userData.boost.value).toBe(null);
+  expect(userData.boost.message).toBe('Você não possui turbo disponível');
+});
